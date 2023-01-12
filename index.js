8389ace592919e9fb2db60b37e780045671486b6
Fabien Marceau <fabien.marceau@gmail.com>
2023-01-12 14:27:42 +0100

    Ok fonctionne sur le first practice, mais à refactoriser et code à simplifier

--- index.js
+++ index.js
@@ -12,10 +12,13 @@ let liste = document.getElementsByClassName("ListeElements");
 let button = document.getElementById("button");
 let input = document.getElementById("input");
 let tableChoose = document.getElementById("tableChoose");
-let questionPracticeOne = document.getElementById("questionPracticeOne");
+let questionFirstPractice = document.getElementById("questionFirstPractice");
 let questionPracticeTwo = document.getElementById("questionPracticeTwo");
 let questionPracticeThree = document.getElementById("questionPracticeThree");
 let questionPracticeFour = document.getElementById("questionPracticeFour");
+let FirstPracticeResponseSend = document.getElementById("FirstPracticeResponseSend");
+let ResponseFirstPractice = document.getElementById("ResponseFirstPractice");
+let responseFirstPractice = document.getElementById("responseFirstPractice");
 
 //Variable pour attribuer numéro au addEventListener..........................................................................................................................................
 let numberOne = 1;
@@ -28,71 +31,70 @@ let numberSeven = 7;
 let numberHeight = 8;
 let numberNine = 9;
 let numberTen = 10;
-let numberTableChoose;
-
+// let numberTableChoose;
 
 //Logique pour proposer un chiifre aléatoire qui sera contenu dans l'operation...........................................................................................................................................
 let arrayNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let mapping = arrayNumber[Math.floor(Math.random() * arrayNumber.length)];
-let total = mapping * numberTableChoose;
+let randomNumber = arrayNumber[Math.floor(Math.random() * arrayNumber.length)];
 
+//Test entre la réponse et randomNumber..................................
+FirstPracticeResponseSend.addEventListener("click", () => {
+  if (randomNumber == ResponseFirstPractice.value) {
+    responseFirstPractice.innerHTML = "Yeah!! Tu gagnes!";
+  } else {
+    responseFirstPractice.innerHTML = "Yeah!! Tu gagnes!";"Uuuurps! T'es vraiment sûr, là?!!";
+  }
+});
 
 //Gestion d'évenement en attribuant le numéro dans la variable, correspondant au numéro du bouton cliqué..........................................................................................................................................
 one.addEventListener("click", () => {
   tableChoose.innerHTML = "Ok!! Tu as choisi la table de " + numberOne;
-  let totalOne = mapping * numberOne;
-  questionPracticeOne.innerHTML = `? x ${numberOne} = ${totalOne}`;
-
+  let totalOne = randomNumber * numberOne;
+  questionFirstPractice.innerHTML = `? x ${numberOne} = ${totalOne}`;
 });
 two.addEventListener("click", () => {
   tableChoose.innerHTML = "Ok!! Tu as choisi la table de " + numberTwo;
-    let totalTwo = mapping * numberTwo;
-    questionPracticeOne.innerHTML = `? x ${numberTwo} = ${totalTwo}`;
+  let totalTwo = randomNumber * numberTwo;
+  questionFirstPractice.innerHTML = `? x ${numberTwo} = ${totalTwo}`;
 });
 three.addEventListener("click", () => {
   tableChoose.innerHTML = "Ok!! Tu as choisi la table de " + numberThree;
-     let totalThree = mapping * numberThree;
-     questionPracticeOne.innerHTML = `? x ${numberThree} = ${totalThree}`;
+  let totalThree = randomNumber * numberThree;
+  questionFirstPractice.innerHTML = `? x ${numberThree} = ${totalThree}`;
 });
 four.addEventListener("click", () => {
   tableChoose.innerHTML = "Ok!! Tu as choisi la table de " + numberFour;
-     let totalFour = mapping * numberFour;
-     questionPracticeOne.innerHTML = `? x ${numberFour} = ${totalFour}`;
+  let totalFour = randomNumber * numberFour;
+  questionFirstPractice.innerHTML = `? x ${numberFour} = ${totalFour}`;
 });
 five.addEventListener("click", () => {
   tableChoose.innerHTML = "Ok!! Tu as choisi la table de " + numberFive;
-     let totalFive = mapping * numberFive;
-     questionPracticeOne.innerHTML = `? x ${numberFive} = ${totalFive}`;
+  let totalFive = randomNumber * numberFive;
+  questionFirstPractice.innerHTML = `? x ${numberFive} = ${totalFive}`;
 });
 six.addEventListener("click", () => {
   tableChoose.innerHTML = "Ok!! Tu as choisi la table de " + numberSix;
-     let totalSix = mapping * numberSix;
-     questionPracticeOne.innerHTML = `? x ${numberSix} = ${totalSix}`;
+  let totalSix = randomNumber * numberSix;
+  questionFirstPractice.innerHTML = `? x ${numberSix} = ${totalSix}`;
 });
 seven.addEventListener("click", () => {
   tableChoose.innerHTML = "BOk!!Tu as choisi la table de " + numberSeven;
-     let totalSeven = mapping * numberSeven;
-     questionPracticeOne.innerHTML = `? x ${numberSeven} = ${totalSeven}`;
+  let totalSeven = randomNumber * numberSeven;
+  questionFirstPractice.innerHTML = `? x ${numberSeven} = ${totalSeven}`;
 });
 height.addEventListener("click", () => {
   tableChoose.innerHTML = "Ok!! Tu as choisi la table de " + numberHeight;
-     let totalHeight = mapping * numberHeight;
-     questionPracticeOne.innerHTML = `? x ${numberHeight} = ${totalHeight}`;
+  let totalHeight = randomNumber * numberHeight;
+  questionFirstPractice.innerHTML = `? x ${numberHeight} = ${totalHeight}`;
 });
 
 nine.addEventListener("click", () => {
   tableChoose.innerHTML = "Ok!! Tu as choisi la table de " + numberNine;
-     let totalNine = mapping * numberNine;
-     questionPracticeOne.innerHTML = `? x ${numberNine} = ${totalNine}`;
+  let totalNine = randomNumber * numberNine;
+  questionFirstPractice.innerHTML = `? x ${numberNine} = ${totalNine}`;
 });
 ten.addEventListener("click", () => {
   tableChoose.innerHTML = "Ok!! Tu as choisi la table de " + numberTen;
-     let totalTen = mapping * numberTen;
-     questionPracticeOne.innerHTML = `? x ${numberTen} = ${totalTen}`;
+  let totalTen = randomNumber * numberTen;
+  questionFirstPractice.innerHTML = `? x ${numberTen} = ${totalTen}`;
 });
-
-
-
-
-
-
